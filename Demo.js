3819d3aa1d32d6ec9a6f296009fb883f755d82ef
Ramu <ramaiahudayagiri89@gmail.com>
2021-03-16 13:06:29 +0530

    initial commit

---
+++ Demo.js
@@ -0,0 +1,19 @@
+import React,{Component}from 'react';
+import {View,Text} from 'react-native';
+
+export default class Demo extends Component
+{
+render()
+{
+  return(
+<View>
+
+<Text>This is screen dcnmdsnj,dfnfsdj,fds 2</Text>
+</View>
+  
+
+
+  );
+}
+
+}
